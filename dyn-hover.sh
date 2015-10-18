c170035df4d4c5ddef6926a9515149ad019bbc58
Philipp Fehre <philipp@fehre.co.uk>
2015-10-18 15:20:54 +0100

    trust certs
    curl on OpenWRT does not necessarily have all the CAs needed so add the
    `-k` to trust the certs we get. Yes we transmit username and password
    here so we should be careful but oh well...

--- dyn-hover.sh
+++ dyn-hover.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 [[ $# -lt 3 ]] && echo "Usage: $0 USERNAME PASSWORD DNS_ID" && exit 1
 
+COOKIEJAR=/tmp/hover_cookie.txt
 USERNAME=${1}
 PASSWORD=${2}
 DNS_ID=${3}
@@ -11,20 +12,8 @@ DNS_ID=${3}
 # (replace "yourdomain.com" with your actual domain, and look for the record
 # you want to change. The ID looks like: dns1234567)
 
-IP=$(curl "http://ifconfig.me/ip" -s)
-
-curl "https://www.hover.com/api/dns/${DNS_ID}" \
-     -X PUT            \
-     -d "content=${IP}" \
-     -s                \
-     -b <(curl "https://www.hover.com/api/login" \
-               -X POST                   \
-               -G                        \
-               -d "username=${USERNAME}" \
-               -d "password=${PASSWORD}" \
-               -s                        \
-               -o /dev/null              \
-               -c -)
-
+IP=$(curl "https://api.ipify.org" -ks)
+curl -k "https://www.hover.com/api/login" -X POST -G -d "username=${USERNAME}" -d "password=${PASSWORD}" -s -o /dev/null -c $COOKIEJAR
+curl -k "https://www.hover.com/api/dns/${DNS_ID}" -X PUT -d "content=${IP}" -s -b $COOKIEJAR
 echo
 
